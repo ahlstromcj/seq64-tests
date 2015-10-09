8e55d9aab2fbb1ed060e76c900070c7968f20c96
Chris Ahlstrom <ahlstrom@centiped>
2015-10-09 13:58:08 -0400

    Updating click tests.

--- libseq64/src/click_unit_test.cpp
+++ libseq64/src/click_unit_test.cpp
@@ -23,6 +23,7 @@
  *    -# modifier(), mod_control(), mod_control_shift(), and mod_super()
  */
 
+#include "click.hpp"                   /* seq64::click class                  */
 #include "click_unit_test.hpp"
 
 /**
@@ -90,13 +91,23 @@ click_unit_test_01_01 (const xpc::cut_options & options)
 }
 
 /**
- *    Create a click and test it.
+ *    Check the given click and test it against the given (and expected)
+ *    parameters. Note that the defaults of the expected parameters match
+ *    the values assumed by the default click constructor.  This match is
+ *    set up for convenience in testing.
  */
 
 static bool
-create_and_check_click (int x, int y, int button, bool press, int modkey)
+check_click
+(
+   const seq64::click & c,
+   int x = CLICK_X_MIN,
+   int y = CLICK_Y_MIN,
+   int button = CLICK_BUTTON_LEFT,
+   bool press = false,
+   int modkey = int(seq64::SEQ64_NO_MASK)
+)
 {
-   seq64::click c(x, y, button, press, seq64::seq_modifier_t(modkey));
    bool result = false;
    if (x < CLICK_X_MIN || x >= CLICK_X_MAX)
       result = c.x() == CLICK_BAD_VALUE;
@@ -186,6 +197,42 @@ create_and_check_click (int x, int y, int button, bool press, int modkey)
 }
 
 /**
+ *    Check an existing click against itself and another one.  Use for
+ *    checking the copy constructor and principal assignment operator.
+ *    Note that the comparison does not use an operator ==(), since there is
+ *    none defined for seq64::click -- none is needed.
+ */
+
+static bool
+compare_clicks
+(
+   const seq64::click & c1,
+   const seq64::click & c2
+)
+{
+   bool result = check_click(c1);
+   if (result)
+      result = check_click(c2);
+
+   if (result)
+   {
+      result = c1.is_press() == c2.is_press();
+      if (result)
+         result = c1.x() == c2.x();
+
+      if (result)
+         result = c1.y() == c2.y();
+
+      if (result)
+         result = c1.button() == c2.button();
+
+      if (result)
+         result = c1.modifier() == c2.modifier();
+   }
+   return result;
+}
+
+/**
  *    Provides a list of click x-values to test; the full range is a bit much.
  */
 
@@ -254,17 +301,31 @@ static seq64::seq_modifier_t s_mod_list [] =
 static int s_mod_list_size = sizeof(s_mod_list) / sizeof(seq64::seq_modifier_t);
 
 /**
- *    Passes a wide range of parameters to the principal constructor
- *    for a complete test.
+ *    Indicates which constructor is to be tested.  We could test them all
+ *    at once, but would like to be able to pinpoint which one, if any,
+ *    failed during a test.
+ */
+
+enum click_test_t
+{
+   CT_PRINCIPAL_CONSTRUCTOR,
+   CT_COPY_CONSTRUCTOR,
+   CT_PRINCIPAL_ASSIGNMENT_OPERATOR
+};
+
+/**
+ *    Passes a wide range of parameters to the principal constructor, copy
+ *    constructor, and principle assignment operator for a complete-enough
+ *    test of these functions.
  */
 
 static bool
-click_principal_ctor_tests ()
+click_ctor_tests (click_test_t testitem = CT_PRINCIPAL_CONSTRUCTOR)
 {
    bool result;
-   for (int xcount = 0; xcount < s_x_list_size; ++xcount)
+   for (int xi = 0; xi < s_x_list_size; ++xi)
    {
-      for (int ycount = 0; ycount < s_y_list_size; ++ycount)
+      for (int yi = 0; yi < s_y_list_size; ++yi)
       {
          for
          (
@@ -276,13 +337,31 @@ click_principal_ctor_tests ()
             for (int i = 0; i <= 1; ++i)
             {
                bool press = i == 1;
-               for (int modcount = 0; modcount < s_mod_list_size; ++modcount)
+               for (int m = 0; m < s_mod_list_size; ++m)
                {
-                  result = create_and_check_click
-                  (
-                     s_x_list[xcount], s_x_list[ycount], button, press,
-                     s_mod_list[modcount]
-                  );
+                  int x = s_x_list[xi];
+                  int y = s_y_list[yi];
+                  seq64::seq_modifier_t smt = s_mod_list[m];
+                  seq64::click c(x, y, button, press, smt);
+                  if (testitem == CT_PRINCIPAL_CONSTRUCTOR)
+                     result = check_click(c, x, y, button, press, smt);
+                  else if (testitem == CT_COPY_CONSTRUCTOR)
+                  {
+                     seq64::click c2(c);                    /* copy object    */
+                     result = compare_clicks(c, c2);
+                  }
+                  else if (testitem == CT_PRINCIPAL_ASSIGNMENT_OPERATOR)
+                  {
+                     seq64::click c2;
+                     /*
+                      * Make sure the compiler doesn't "optimize" assignment
+                      * into a copy constructor, unlikely though that may
+                      * be.
+                      */
+
+                     (void) c2.operator =(c);
+                     result = compare_clicks(c, c2);
+                  }
                   if (! result)
                      break;
                }
@@ -336,7 +415,139 @@ click_unit_test_01_02 (const xpc::cut_options & options)
       {
          if (status.next_subtest("click::click()"))
          {
-            ok = click_principal_ctor_tests();
+            ok = click_ctor_tests();
+            status.pass(ok);
+         }
+      }
+   }
+   return status;
+}
+
+/**
+ * \group
+ *    1. seq64::click
+ *
+ * \case
+ *    3. Copy constructor
+ *
+ * \tests
+ *    -  seq64::click::click(click)
+ *
+ * \param options
+ *    Provides the command-line options for the unit-test application.
+ *
+ * \return
+ *    Returns the unit-test status object needed by the protocol.
+ */
+
+xpc::cut_status
+click_unit_test_01_03 (const xpc::cut_options & options)
+{
+   xpc::cut_status status
+   (
+      options, 1, 3, "seq64::click", _("Copy constructor")
+   );
+   bool ok = status.valid();                       /* invalidity not an error */
+   if (ok)
+   {
+      if (! status.can_proceed())                  /* is test allowed to run? */
+      {
+         status.pass();                            /* no, force it to pass    */
+      }
+      else
+      {
+         if (status.next_subtest("Default"))
+         {
+            seq64::click c1;
+            seq64::click c2;
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("Copy default"))
+         {
+            seq64::click c1;
+            seq64::click c2(c1);
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("Assign default"))
+         {
+            seq64::click c1;
+            seq64::click c2;
+            c2 = c1;
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("operator =()"))
+         {
+            seq64::click c1;
+            seq64::click c2;
+            (void) c2.operator =(c1);
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("Copy principal"))
+         {
+            ok = click_ctor_tests(CT_COPY_CONSTRUCTOR);
+            status.pass(ok);
+         }
+      }
+   }
+   return status;
+}
+
+/**
+ * \group
+ *    1. seq64::click
+ *
+ * \case
+ *    4. Principal assignment operator
+ *
+ * \tests
+ *    -  seq64::click::operator =(click)
+ *
+ * \param options
+ *    Provides the command-line options for the unit-test application.
+ *
+ * \return
+ *    Returns the unit-test status object needed by the protocol.
+ */
+
+xpc::cut_status
+click_unit_test_01_04 (const xpc::cut_options & options)
+{
+   xpc::cut_status status
+   (
+      options, 1, 4, "seq64::click", _("Principal assignment operator")
+   );
+   bool ok = status.valid();                       /* invalidity not an error */
+   if (ok)
+   {
+      if (! status.can_proceed())                  /* is test allowed to run? */
+      {
+         status.pass();                            /* no, force it to pass    */
+      }
+      else
+      {
+         if (status.next_subtest("Assign default"))
+         {
+            seq64::click c1;
+            seq64::click c2;
+            c2 = c1;
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("operator =()"))
+         {
+            seq64::click c1;
+            seq64::click c2;
+            (void) c2.operator =(c1);
+            ok = compare_clicks(c1, c2);
+            status.pass(ok);
+         }
+         if (status.next_subtest("Assign principal"))
+         {
+            ok = click_ctor_tests(CT_PRINCIPAL_ASSIGNMENT_OPERATOR);
             status.pass(ok);
          }
       }
--- libseq64/src/libseq64_unit_test.cpp
+++ libseq64/src/libseq64_unit_test.cpp
@@ -3,7 +3,7 @@
  * \library       libseq64 (from the Sequencer64 project)
  * \author        Chris Ahlstrom
  * \date          2015-10-06
- * \updates       2015-10-08
+ * \updates       2015-10-09
  * \version       $Revision$
  * \license       $XPC_SUITE_GPL_LICENSE$
  *
@@ -83,8 +83,9 @@ SEQ64_TEST_NAME "-specific options:\n"
       if (load_the_tests)
       {
          ok = testbattery.load(click_unit_test_01_01);
-         if (ok)
-            ok = testbattery.load(click_unit_test_01_02);
+         if (ok) ok = testbattery.load(click_unit_test_01_02);
+         if (ok) ok = testbattery.load(click_unit_test_01_03);
+         if (ok) ok = testbattery.load(click_unit_test_01_04);
       }
       if (ok)
          ok = testbattery.run();
